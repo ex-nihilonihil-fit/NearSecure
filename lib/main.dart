8f48d10d80f740a5a713e30e4921b5586028b382
janani-manoj <131209830+janani-manoj@users.noreply.github.com>
2023-08-01 10:54:01 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:latlng/latlng.dart';
 import 'package:rf_block/database_services/database_display.dart';
+import 'package:rf_block/location_services/display_location_data.dart';
 import 'package:rf_block/nfc_services/nfc_listener.dart';
 import 'package:rf_block/nfc_services/nfc_transmitter.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'database_services/database_helper.dart';
-import 'location_services/display_location_data.dart';
 import 'flutter_flow/flutter_flow_icon_button.dart';
 import 'flutter_flow/flutter_flow_theme.dart';
-import 'flutter_flow/flutter_flow_util.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter_map/plugin_api.dart';
-
-
 
 
 var nfcListener = NFCListener();
 var nfcTransmitter = NFCTransmitter();
+List<LatLng> locationsList =  [];
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -436,9 +428,15 @@ class HomePage extends StatelessWidget {
                             color: FlutterFlowTheme.of(context).primaryBtnText,
                             size: 26,
                           ),
-                          onPressed: () {
-                            // navigate to the DisplayLocationData view
-                            Navigator.push(context, MaterialPageRoute(builder: (context) =>  DisplayLocation()));
+                          onPressed: () async {
+                            locationsList =  await DatabaseDisplay().getLocation() as List<LatLng>;
+                            if(locationsList.length == 0){
+                              Navigator.push(context, MaterialPageRoute(builder: (context) =>  NoDataDisplay()));
+                            } else {
+                              // navigate to the DisplayLocationData view
+                              Navigator.push(context, MaterialPageRoute(builder: (context) =>  DisplayLocation()));
+                            }
+
                           },
                         ),
                       ),
@@ -613,221 +611,3 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class DisplayLogData extends StatefulWidget {
-  DisplayLogData({Key? key}) : super(key: key);
-
-  @override
-  State<DisplayLogData> createState() => _DisplayLogData();
-}
-class _DisplayLogData extends State<DisplayLogData> {
-
-  List<String> _logs = [];
-  @override
-  initState(){
-    _updateLog();
-    super.initState();
-  }
-
-  Future<void> _updateLog() async {
-    String data = await DatabaseDisplay().getData();
-    setState((){
-      _logs = data.split('\n');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: FlutterFlowTheme.of(context).lineColor,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(110),
-        child: AppBar(
-          backgroundColor: FlutterFlowTheme.of(context).lineColor,
-          automaticallyImplyLeading: false,
-          actions: [],
-          flexibleSpace: FlexibleSpaceBar(
-            background: Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(2, 5, 2, 0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset(
-                  'images/logs.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          centerTitle: false,
-        ),
-      ),
-      body: _logs==null ? Container():ListView.builder(
-        itemCount: _logs.length,
-        itemBuilder: (_, int position) {
-          return Card(
-            child: ListTile(
-              title:
-              Text(_logs[position]),
-            ),
-          );
-        },
-      ),
-    ); // Scaffold
-  }
-
-}
-
-class DisplayLocation extends StatefulWidget {
-
-  DisplayLocation({Key? key}) : super(key: key);
-  @override
-  _DisplayLocation createState() => _DisplayLocation();
-}
-class _DisplayLocation extends State<DisplayLocation>  {
-
-  List<LatLng> locations = [LatLng(37.354107, -121.955238)];
-  Future<List<LatLng>> getLocation() async {
-    // get the data from the database
-    List<Map> list = await databaseHelper.queryLoc();
-    String data = '';
-    List<String> locs = [];
-    // convert the data to a list of LatLng objects
-    for (int i = 0; i < list.length; i++) {
-      data = '$data${list[i]}\n';
-    }
-    locs = data.split('\n');
-    for (int j = 0; j < (locs.length - 1); j++) {
-      List<String> temp = locs[j].split(' ');
-      locations.add(LatLng(double.parse(temp[1]), double.parse(temp[2].replaceAll('}', ''))));
-    }
-    locations.removeAt(0);
-    // return the data
-    return locations;
-  }
-
-  MapController _mController = MapController();
-  List<Marker> _markerList = [];
-
-  @override
-  void initState() {
-    getLocation();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: FlutterFlowTheme.of(context).lineColor,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(110),
-          child: AppBar(
-            backgroundColor: FlutterFlowTheme.of(context).lineColor,
-            automaticallyImplyLeading: false,
-            actions: [],
-            flexibleSpace: FlexibleSpaceBar(
-              background: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(2, 5, 2, 0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.asset(
-                    'images/location.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            centerTitle: false,
-          ),
-        ),
-        body: displayLocationData()
-    );
-  }
-  Widget displayLocationData() {
-    return FutureBuilder(
-      future: getLocation(),
-
-      builder: (BuildContext context, AsyncSnapshot<List<LatLng>> snapshot) {
-        if (snapshot.hasData) {
-          return FlutterMap(
-            mapController: _mController,
-            options: MapOptions(
-              center: snapshot.data![0],
-              bounds: LatLngBounds.fromPoints(snapshot.data!),
-              zoom: 3,
-            ),
-            nonRotatedChildren: [
-              RichAttributionWidget(
-                attributions: [
-                  TextSourceAttribution(
-                    'OpenStreetMap contributors',
-                    onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-                  ),
-                ],
-              ),
-            ],
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                //userAgentPackageName: 'com.example.app',
-              ),
-              MarkerClusterLayerWidget(options: MarkerClusterLayerOptions(
-                maxClusterRadius: 190,
-                disableClusteringAtZoom: 16,
-                size: Size(50, 50),
-                fitBoundsOptions: FitBoundsOptions(
-                  padding: EdgeInsets.all(50),
-                ),
-                markers: snapshot.data!.map((point) => Marker(
-                  point: point,
-                  width: 60,
-                  height: 60,
-                  builder: (context) => Icon(
-                    Icons.pin_drop,
-                    size: 60,
-                    color: Colors.blueAccent,
-                  ),
-                )).toList(),
-                polygonOptions: PolygonOptions(
-                    borderColor: Colors.blueAccent,
-                    color: Colors.black12,
-                    borderStrokeWidth: 3),
-                builder: (context, markers) {
-                  return Container(
-                    alignment: Alignment.center,
-                    decoration:
-                    BoxDecoration(color: Color(0xFF1CA8F1), shape: BoxShape.circle),
-                    child: Text('${markers.length-1}'),
-                  );
-                },
-              ),)
-            ],
-          );
-        } else {
-          return FlutterMap(
-            mapController: _mController,
-            options: MapOptions(
-              center: LatLng(37.354107, -121.955238),
-              //bounds: LatLngBounds.fromPoints(_latLongList),
-              zoom: 3,
-            ),
-            nonRotatedChildren: [
-              RichAttributionWidget(
-                attributions: [
-                  TextSourceAttribution(
-                    'OpenStreetMap contributors',
-                    onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-                  ),
-                ],
-              ),
-            ],
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                //userAgentPackageName: 'com.example.app',
-              ),
-            ],
-          );
-        }
-      },
-    );
-  }
-}
