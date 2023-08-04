1a021d11529b2b049d4a6c500c6459c788aecc5a
ex-nihilonihil-fit <44554771+ex-nihilonihil-fit@users.noreply.github.com>
2023-08-03 22:32:22 -0700

    Update nfc_transmitter.dart

--- lib/nfc_services/nfc_transmitter.dart
+++ lib/nfc_services/nfc_transmitter.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 import 'package:ndef/ndef.dart' as ndef;
@@ -10,46 +9,18 @@ import 'dart:async';
 class NFCTransmitter {
   get urlRecords => null;
 
-  Future<void> transmit() async {
-    var availability = await FlutterNfcKit.nfcAvailability;
-    if (availability != NFCAvailability.available) {
-      //display the error
-      print('NFC is not available on this device'); //TODO: Remove this line
-    } else {
-      print('NFC Transmitter started'); //TODO: Remove this line
-    }
-    // listen for any tag and get the tag data when it's tapped.
-    var tag = await FlutterNfcKit.poll(timeout: const Duration(seconds: 240));
-    var id = tag.id;
-    print('NFC ID: $id'); //TODO: Remove this line
-    var nfcData = '${tag.type} ${tag.sak} ${tag.atqa}';
-    print('NFC Data: $nfcData'); //TODO: Remove this line
-
-    // if a tag is found, write random data to it
-    var randomNumber = RandomData().getRandom32Bits();
-    // write the random string to the tag
-    //await FlutterNfcKit.writeNDEFRecords([ndef.UriRecord.fromString(randomNumber.toString())]);
-    // handle the PlatformException error since the tag isn't formatted and the write will fail
-    try {
-      await FlutterNfcKit.writeNDEFRecords([ndef.UriRecord.fromString(randomNumber.toString())]);
-    } on PlatformException catch (e) {
-      print('Error: $e'); //TODO: Remove this line
-    }
-
-
-    print('random number: $randomNumber written to tag'); //TODO: Remove this line
-
-    // get the current location
-    var location = await LocationData().getLocation();
-    print('location: $location'); //TODO: Remove this line
-
-    // get the current time without seconds and milliseconds
-    var time = DateTime.now().toString().substring(0, 16);
-    print('time: $time'); //TODO: Remove this line
-
-    // Write the data to the database.
-    DatabaseHelper().insert(time.replaceAll(' ', '@'), id, nfcData.replaceAll(',', ''), location.replaceAll(',', ''));
+void transmit() {
+  var randomNumber = RandomData().getRandom32Bits();
+  // write the random string to the tag
+  //await FlutterNfcKit.writeNDEFRecords([ndef.UriRecord.fromString(randomNumber.toString())]);
+  // handle the PlatformException error since the tag isn't formatted and the write will fail
+  try {
+    FlutterNfcKit.writeNDEFRecords(
+        [ndef.UriRecord.fromString(randomNumber.toString())]);
+  } on PlatformException catch (e) {
+    print('Error: $e'); //TODO: Remove this line
   }
+}
 
   // close the session
   Future<void> stop() async {
